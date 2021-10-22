6ac4b5aa0138c8f6bfb8f54fd997cdf4062b4589
TheHatedGravity <nicwba@gmail.com>
2021-10-23 09:29:21 +1300

    Add bloom intensity slider for Prime 3. Remove Prime 3 from 'Disable Bloom' toggle and allow 'Reduce Bloom Offset' and 'Disable Bloom' to be activated simultaneously.

--- Source/Core/DolphinQt/Config/Graphics/PrimeWidget.cpp
+++ Source/Core/DolphinQt/Config/Graphics/PrimeWidget.cpp
@@ -28,8 +28,7 @@
 #include "Core/PrimeHack/HackConfig.h"
 #include "Core/Core.h"
 
-PrimeWidget::PrimeWidget(GraphicsWindow* parent)
-{
+PrimeWidget::PrimeWidget(GraphicsWindow* parent) {
   CreateWidgets();
   ConnectWidgets();
   AddDescriptions();
@@ -38,11 +37,9 @@ PrimeWidget::PrimeWidget(GraphicsWindow* parent)
   ArmPositionModeChanged(m_manual_arm_position->isChecked());
 
   m_select_colour->setStyleSheet(tr("border: 1px solid #") + QString::number(Config::Get(Config::GC_CROSSHAIR_COLOR_RGBA) & 0xFFFFFF00, 16) + tr(";"));
-  m_reduce_bloom->setEnabled(!m_disable_bloom->isChecked()); 
 }
 
-void PrimeWidget::CreateWidgets()
-{
+void PrimeWidget::CreateWidgets() {
   auto* main_layout = new QVBoxLayout;
 
   // Graphics
@@ -52,8 +49,6 @@ void PrimeWidget::CreateWidgets()
   graphics_box->setLayout(graphics_layout);
 
   m_autoefb = new GraphicsBool(tr("Auto Toggle EFB copies to Texture"), Config::AUTO_EFB);
-  m_disable_bloom = new GraphicsBool(tr("Disable Bloom"), Config::DISABLE_BLOOM);
-  m_reduce_bloom = new GraphicsBool(tr("Reduce Bloom [Metroid Prime 3]"), Config::REDUCE_BLOOM);
   m_motions_lock = new GraphicsBool(tr("Lock Camera in Motion Puzzles"), Config::LOCKCAMERA_IN_PUZZLES);
   m_toggle_arm_position = new GraphicsBool(tr("Toggle Viewmodel Adjustment"), Config::TOGGLE_ARM_REPOSITION);
   m_toggle_culling = new GraphicsBool(tr("Disable Culling"), Config::TOGGLE_CULLING);
@@ -63,12 +58,11 @@ void PrimeWidget::CreateWidgets()
   graphics_layout->addWidget(m_autoefb, 0, 0);
   graphics_layout->addWidget(m_motions_lock, 1, 0);
   graphics_layout->addWidget(m_toggle_secondaryFX, 2, 0);
-  graphics_layout->addWidget(m_disable_bloom, 3, 0);
-  graphics_layout->addWidget(m_reduce_bloom, 4, 0);
-  graphics_layout->addWidget(m_toggle_arm_position, 5, 0);
-  graphics_layout->addWidget(m_toggle_culling, 6, 0);
+  graphics_layout->addWidget(m_toggle_arm_position, 3, 0);
+  graphics_layout->addWidget(m_toggle_culling, 4, 0);
 
   m_fov_axis = new GraphicsSlider(1, 170, Config::FOV);
+  m_fov_axis->setMaximumWidth(200);
   m_fov_axis->setMinimum(1);
   m_fov_axis->setMaximum(170);
   m_fov_axis->setPageStep(1);
@@ -76,11 +70,33 @@ void PrimeWidget::CreateWidgets()
   fov_counter = new GraphicsInteger(1, 170, Config::FOV);
   fov_counter->setMaximumWidth(50);
 
-  graphics_layout->addItem(new QSpacerItem(1, 5), 7, 0);
-  graphics_layout->addWidget(new QLabel(tr("Field of View")), 8, 0);
-  graphics_layout->addWidget(m_fov_axis, 8, 1);
-  graphics_layout->addWidget(fov_counter, 8, 2);
+  graphics_layout->addItem(new QSpacerItem(1, 5), 5, 0);
+  graphics_layout->addWidget(new QLabel(tr("Field of View")), 6, 0);
+  graphics_layout->addWidget(m_fov_axis, 6, 1);
+  graphics_layout->addWidget(fov_counter, 6, 2);
 
+  //Bloom
+  auto* bloom_box = new QGroupBox(tr("Bloom"));
+  auto* bloom_layout = new QGridLayout();
+
+  bloom_box->setLayout(bloom_layout);
+
+  m_disable_bloom = new GraphicsBool(tr("Disable Bloom [Prime 1, 2]"), Config::DISABLE_BLOOM);
+  m_reduce_bloom = new GraphicsBool(tr("Reduce Bloom Offset [Prime 3]"), Config::REDUCE_BLOOM);
+  bloom_intensity_val = new GraphicsInteger(0, 100, Config::BLOOM_INTENSITY);
+  bloom_intensity_val->setMaximumWidth(50);
+  m_bloom_intensity = new GraphicsSlider(0, 100, Config::BLOOM_INTENSITY);
+  m_bloom_intensity->setMaximumWidth(200);
+  m_bloom_intensity->setMinimum(0);
+  m_bloom_intensity->setMaximum(100);
+  m_bloom_intensity->setPageStep(1);
+
+  bloom_layout->addWidget(m_disable_bloom, 1, 0);
+  bloom_layout->addWidget(m_reduce_bloom, 2, 0);
+  bloom_layout->addWidget(new QLabel(tr("Bloom Intensity [Prime 3]")), 3, 0);
+  bloom_layout->addWidget(m_bloom_intensity, 3, 1);
+  bloom_layout->addWidget(bloom_intensity_val, 3, 2);
+  
   // Crosshair Color
   auto* crosshair_color_box = new QGroupBox(tr("GCN Crosshair Color"));
   auto* crosshair_color_layout = new QHBoxLayout();
@@ -119,14 +135,17 @@ void PrimeWidget::CreateWidgets()
   y_counter = new GraphicsInteger(-50, 50, Config::ARMPOSITION_UPDOWN);
   y_counter->setMaximumWidth(50);
 
+  m_x_axis->setMaximumWidth(200);
   m_x_axis->setMinimum(-50);
   m_x_axis->setMaximum(50);
   m_x_axis->setPageStep(1);
 
+  m_z_axis->setMaximumWidth(200);
   m_z_axis->setMinimum(-50);
   m_z_axis->setMaximum(50);
   m_z_axis->setPageStep(1);
 
+  m_y_axis->setMaximumWidth(200);
   m_y_axis->setMinimum(-50);
   m_y_axis->setMaximum(50);
   m_y_axis->setPageStep(1);
@@ -147,6 +166,7 @@ void PrimeWidget::CreateWidgets()
   viewmodel_layout->addWidget(y_counter, 5, 2);
   
   main_layout->addWidget(graphics_box);
+  main_layout->addWidget(bloom_box);
   main_layout->addWidget(crosshair_color_box);
   main_layout->addWidget(viewmodel_box);
   main_layout->addStretch();
@@ -154,8 +174,7 @@ void PrimeWidget::CreateWidgets()
   setLayout(main_layout);
 }
 
-void PrimeWidget::ArmPositionModeChanged(bool mode)
-{
+void PrimeWidget::ArmPositionModeChanged(bool mode) {
   m_x_axis->setEnabled(mode);
   m_y_axis->setEnabled(mode);
   m_z_axis->setEnabled(mode);
@@ -165,13 +184,11 @@ void PrimeWidget::ArmPositionModeChanged(bool mode)
   z_counter->setEnabled(mode);
 }
 
-void PrimeWidget::ToggleShowCrosshair(bool mode)
-{
+void PrimeWidget::ToggleShowCrosshair(bool mode) {
   m_select_colour->setEnabled(mode);
 }
 
-void PrimeWidget::ConnectWidgets()
-{
+void PrimeWidget::ConnectWidgets() {
   connect(m_toggle_gc_show_crosshair, &GraphicsBool::clicked, this, [=](bool checked) {PrimeWidget::ToggleShowCrosshair(checked); });
   connect(m_auto_arm_position, &QRadioButton::clicked, this, [=](bool checked) {PrimeWidget::ArmPositionModeChanged(!checked);});
   connect(m_manual_arm_position, &QRadioButton::clicked, this, [=](bool checked) {PrimeWidget::ArmPositionModeChanged(checked);});
@@ -181,11 +198,6 @@ void PrimeWidget::ConnectWidgets()
       m_auto_arm_position->setEnabled(checked); 
       m_manual_arm_position->setEnabled(checked); 
       PrimeWidget::ArmPositionModeChanged(m_manual_arm_position->isChecked());
-    });
-  connect(m_disable_bloom, &QCheckBox::clicked, this,
-    [=](bool checked)
-    {
-      m_reduce_bloom->setEnabled(!checked); 
     });
   connect(&Settings::Instance(), &Settings::EmulationStateChanged, this,
     [=](Core::State state)
@@ -223,20 +235,22 @@ void PrimeWidget::LoadSettings() {}
 
 void PrimeWidget::SaveSettings() {}
 
-void PrimeWidget::AddDescriptions()
-{
+void PrimeWidget::AddDescriptions() {
   static const char TR_AUTO_EFB[] =
     QT_TR_NOOP("Automatically disables 'Store EFB Copies to Texture Only'"
-      "while the Scan Visor is active in Metroid Prime 2 and Metroid Prime 3. \n\n"
-      "While 'Store EFB Copies to Texture Only' may improve performance, having it "
-      "enabled will break the scan visor in Metroid Prime 2 and Metroid Prime 3.");
+               "while the Scan Visor is active in Metroid Prime 2 and Metroid Prime 3. \n\n"
+               "While 'Store EFB Copies to Texture Only' may improve performance, having it "
+               "enabled will break the scan visor in Metroid Prime 2 and Metroid Prime 3.");
   static const char TR_GUNEFFECTS[] =
     QT_TR_NOOP("Reintroduce the original secondary gun effects that were in the GameCube version of Metroid Prime 1\n"
                 "These effects were disabled and cut in the Trilogy but still remained as unused assets.");
   static const char TR_DISABLE_BLOOM[] =
-    QT_TR_NOOP("Disables Bloom.\n\nSource: TheHatedGravity and dreamsyntax.");
+    QT_TR_NOOP("Disables bloom.\n\nSource: TheHatedGravity and dreamsyntax.");
   static const char TR_REDUCE_BLOOM[] =
-    QT_TR_NOOP("Reduces Bloom. Upscaling the resolution massively exaggerates the bloom, especially in Prime 3. This option fixes this.");
+    QT_TR_NOOP("Reduces bloom offset. Upscaling the resolution massively exaggerates the bloom in Prime 3. This option fixes this.");
+  static const char TR_BLOOM_INTENSITY[] =
+    QT_TR_NOOP("Controls the intensity of the bloom effect in Prime 3. Default value is 100, recommended value is 65, 0 will effectively disable bloom.\n\n"
+               "Source: TheHatedGravity and dreamsyntax.");
   static const char TR_TOGGLE_ARM_POSITION[] =
     QT_TR_NOOP("Toggles repositioning of Samus's arms in the viewmodel. Repositioning her arms is visually beneficial for high Field Of Views.");
   static const char TR_TOGGLE_CULL[] =
@@ -257,13 +271,14 @@ void PrimeWidget::AddDescriptions()
   static const char TR_FOV[] =
     QT_TR_NOOP("Modifies the Field of View. The Prime games are not designed to go beyond 101 FOV (94 in Prime 3), so if you do PrimeHack has to "
                "disable culling and modify the znear values in the game. The higher the FOV, the more glitches you may encounter."
-                "\n\nGenerally the best FOV values are between 75 and 100.");
+               "\n\nGenerally the best FOV values are between 75 and 100.");
 
   m_autoefb->SetDescription(tr(TR_AUTO_EFB));
   m_motions_lock->SetDescription(tr(TR_MOTION_LOCK));
   m_toggle_secondaryFX->SetDescription(tr(TR_GUNEFFECTS));
   m_disable_bloom->SetDescription(tr(TR_DISABLE_BLOOM));
   m_reduce_bloom->SetDescription(tr(TR_REDUCE_BLOOM));
+  bloom_intensity_val->SetDescription(tr(TR_BLOOM_INTENSITY));
   m_toggle_culling->SetDescription(tr(TR_TOGGLE_CULL));
   m_toggle_arm_position->SetDescription(tr(TR_TOGGLE_ARM_POSITION));
   m_manual_arm_position->SetDescription(tr(TR_MANUAL_POSITION));
